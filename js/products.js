2cded4b7e54fbc821fe660c379c41fafa7ca0152
michelshako99-debug <michelshako99@gmail.com>
2026-09-02 08:41:44 +0100

    Modify image filenames for Chaussures Enfant
    Updated image filenames for children's shoes.

--- js/products.js
+++ js/products.js
@@ -58,8 +58,8 @@ const products = [
     { id: 62, name: "Ensemble Bébé", category: "Enfant", subcategory: "Bébés", price: 18000, oldPrice: 0, img: "bébé (2).jpg", badge: "new" },
     { id: 63, name: "Ensemble Bébé", category: "Enfant", subcategory: "Bébés", price: 10000, oldPrice: 0, img: "bébé (3).jpg", badge: "" },
     { id: 64, name: "Ensemble Bébé", category: "Enfant", subcategory: "Bébés", price: 16000, oldPrice: 0, img: "bébé (4).jpg", badge: "" },
-    { id: 65, name: "Chaussures Enfant", category: "Enfant", subcategory: "Chaussures", price: 10000, oldPrice: 0, img: "basketkids (A).jpg", badge: "" },
-    { id: 66, name: "Chaussures Enfant", category: "Enfant", subcategory: "Chaussures", price: 7000, oldPrice: 0, img: "basketkids (B).jpg", badge: "" },
+    { id: 65, name: "Chaussures Enfant", category: "Enfant", subcategory: "Chaussures", price: 10000, oldPrice: 0, img: "basketkidsA.jpg", badge: "" },
+    { id: 66, name: "Chaussures Enfant", category: "Enfant", subcategory: "Chaussures", price: 7000, oldPrice: 0, img: "basketkidsB.jpg", badge: "" },
 ];
 
 /* =========================================================
